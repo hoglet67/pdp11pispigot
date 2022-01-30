e2d573bc32c2a0b5dd0b61b88fba630fa42f8e21
David Banks <dave@hoglet.com>
2022-01-30 20:15:44 +0000

    Allow extra to be passed in as third argument

--- test/spigot.c
+++ test/spigot.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-typedef uint32_t thing1;
-typedef uint32_t thing2;
+// Uncomment this to truncate the columns during the calculation
+//#define TRUNCATE_COLUMNS
+
+typedef uint64_t thing1;
+typedef uint64_t thing2;
+
+short extra = 1;
 
 thing1 num_cols(thing1 nchunks, thing1 chunk) {
-   int extra = 2;
-   return (nchunks * chunk + extra) * 10 / 3;
+   return (nchunks * chunk) * 10 / 3 + extra;
 }
 
 int main(int argc, char *argv[]) {
@@ -20,13 +24,15 @@ int main(int argc, char *argv[]) {
    thing1 nines;
    thing1 predigit;
    thing1 nchunks;
-   thing1 extra;
 
    if (argc > 1) {
       ndigits = atoi(argv[1]);
    }
    if (argc > 2) {
       chunk = atoi(argv[2]);
+   }
+   if (argc > 3) {
+      extra = atoi(argv[3]);
    }
    e = 1;
    for (i = 0; i < chunk; i++) {
@@ -46,8 +52,8 @@ int main(int argc, char *argv[]) {
    sprintf(format, "%%.%dd", chunk);
 
 #ifdef DEBUG
-         printf("format = %s, ndigits = %d; chunk = %d; e = %d; n = %d\n",
-             format, ndigits, chunk, e, n);
+   printf("format = %s, ndigits = %d; chunk = %d; extra = %d; e = %d; n = %d\n",
+          format, ndigits, chunk, extra, e, n);
 #endif
 
    // Allocate memory
@@ -65,7 +71,11 @@ int main(int argc, char *argv[]) {
    c = 0;
    for (k = nchunks; k > 0; k -= 1) {
       d = 0;
+#ifdef TRUNCATE_COLUMNS
       i = num_cols(k, chunk);
+#else
+      i = num_cols(nchunks, chunk);
+#endif
       for(;;) {
          d += r[i]*e;
          b = i*2 - 1;
